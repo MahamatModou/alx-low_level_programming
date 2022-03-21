f4977b5c433a25c9152d664f9166be1f7b1d08bf
MahamatModou <mainamht99@gmail.com>
2022-03-21 04:07:07 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 
-void reset_to_98 (int *n){
+int main(void)
+{
+	int n;
 
-n = &n;
+	n = 402;
 
-printf("address of variable n : %x\n", &n);
+printf("address of variable n : %x\n", n);
 
 printf("address stored in m : %x\n", &n);
 
-printf("value of *n : %x\n", &n);
+printf("value of *n : %x\n", n);
 
-return(0);
-}
-
-int main()
-{
-    printf("Hello world!\n");
-
-    reset_to_98(98);
-
-    return 0;
+retrun(0);
 }
