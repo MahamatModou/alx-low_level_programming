f837617f6838007386fd04c9676ab9dd878603b4
MahamatModou <mainamht99@gmail.com>
2022-03-31 08:39:21 -0700

    task 4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+
+int factorial(int n);
+
+if (n < 0)
+	return -1;
+
+else if (n == 0)
+	return 1;
+
+else (n > 0)
+	return n*factorial(n-1);
